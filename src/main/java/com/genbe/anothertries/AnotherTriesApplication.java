7e0723b5f97a9e257b8171ad2ddbd7ce5c215ef2
Nur Alfilail <nur.alfilail96@gmail.com>
2020-08-31 09:11:44 +0700

    initial commit

---
+++ src/main/java/com/genbe/anothertries/AnotherTriesApplication.java
@@ -0,0 +1,13 @@
+package com.genbe.anothertries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherTriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherTriesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/genbe/anothertries/AnotherTriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genbe.anothertries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotherTriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
